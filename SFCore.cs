c4ed1021049f6bc12ba8b45683d5262415fba4ac
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-01-27 17:18:02 +0100

    extract settings classes adjust license year

--- SFCore.cs
+++ SFCore.cs
@@ -51,25 +51,3 @@ public class SFCoreMod : FullSettingsMod<SFCoreSaveSettings, SFCoreGlobalSetting
         SceneManagerPatcher.LoadPrefabs(preloadedObjects["Room_Mender_House"]["_SceneManager"].GetComponent<SceneManager>());
     }
 }
-
-/// <summary>
-/// Global settings for SFCore
-/// </summary>
-public class SFCoreGlobalSettings
-{
-    /// <summary>
-    /// Important to clear and reapply custom charms.
-    /// </summary>
-    public int MaxCustomCharms = 0;
-    /// <summary>
-    /// Important to reapply the wanted menu theme.
-    /// </summary>
-    public string SelectedMenuTheme = "UI_MENU_STYLE_CLASSIC";
-}
-
-/// <summary>
-/// Save specific settings for SFCore
-/// </summary>
-public class SFCoreSaveSettings
-{
-}
