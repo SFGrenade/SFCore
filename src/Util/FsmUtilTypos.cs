3e75c236aaf99060b522464d553b1995ad33b2e5
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2026-03-27 17:05:53 +0100

    add fsmutil file for methods that had typos in the names

---
+++ src/Util/FsmUtilTypos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HutongGames.PlayMaker;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace SFCore.Utils;
+
+public static partial class FsmUtil
+{
+
+    /// <inheritdoc cref="AddGlobalTransition(PlayMakerFSM, string, string)"/>
+    [PublicAPI]
+    [Obsolete("Use method `AddGlobalTransition(PlayMakerFSM, string, string)` instead!")]
+    public static FsmEvent AddFsmGlobalTransitions(this PlayMakerFSM fsm, string globalEventName, string toState) => AddGlobalTransition(fsm, globalEventName, toState);
+
+}
